645bf6b982beffce8bba0cfd66bb460280cfce56
Kafumio <linzehong1_2020@qq.com>
2024-01-03 10:25:40 +0800

    完善utils工具包中的时间工具函数

--- utils/time.go
+++ utils/time.go
@@ -2,4 +2,27 @@ package utils
 
 import "time"
 
+// Time 自定义类型，用于改变time.Time的序列化样式
 type Time time.Time
+
+const (
+	timeFormat = "2006-01-02 15:04:05"
+)
+
+func (t *Time) UnMarshalJson(data []byte) (err error) {
+	now, err := time.ParseInLocation(`"`+timeFormat+`"`, string(data), time.Local)
+	*t = Time(now)
+	return
+}
+
+func (t Time) MarshalJson() ([]byte, error) {
+	b := make([]byte, 0, len(timeFormat)+2)
+	b = append(b, '"')
+	b = time.Time(t).AppendFormat(b, timeFormat)
+	b = append(b, '"')
+	return b, nil
+}
+
+func (t Time) String() string {
+	return time.Time(t).Format(timeFormat)
+}
